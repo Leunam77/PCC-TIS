bd18670a5d9a90ec38f186f08de225ea466bc1b8
olich797 <olich.castro@gmail.com>
2023-10-10 01:59:00 -0400

    front

--- fronted/src/components/ContenedorSlider.js
+++ fronted/src/components/ContenedorSlider.js
@@ -11,9 +11,7 @@ function ContenedorSlider(){
         <div className='containerSlider'>
             <NavbarAdmin/>
             <VisualizarInformacionDeEventosAdmin/>
-            <NavbarUser/>
-            <VisualizarInformacionDeEventosUser/>
-        </div>
+            </div>
     )
 }
 
