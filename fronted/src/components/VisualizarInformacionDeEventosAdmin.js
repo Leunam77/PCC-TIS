57aa77a4bf9c423dd2ca1ed2278c06bf339c50bb
olich797 <olich.castro@gmail.com>
2023-10-11 18:47:25 -0400

    first logo axios

--- fronted/src/components/VisualizarInformacionDeEventosAdmin.js
+++ fronted/src/components/VisualizarInformacionDeEventosAdmin.js
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
 import '../stylesheets/VisualizarInformacionDeEventos.css'
+import axios from 'axios';
+
 function VisualizarInformacionDeEventosAdmin(){
+    
+    const [logos, setLogo] = useState([]);
+    
+    useEffect(() => {
+        //Obtener Datos del logo
+        axios.get('http://')
+          .then((res) => {
+            setLogo(res.data);
+          })
+          .catch((err) => {
+            console.log(err);
+          });
+      }, []);
     return(
         <div className='visualizadorDeEventos'>
             <h1>Taller sobre capacitacion sobre lenguaje C#</h1>
             <div className='content'>
-                <div className='logo'>logo</div>
+                <div className='logo'>
+                    <tbody>
+                        {logos.map((logo) => (
+                        <tr>
+                            key={logo.idlogo}
+                            {logo.imglogo}
+                        </tr>))}
+                    </tbody>
+                </div>
                 <div className='informacion'>
                     <div className='descripcion'>Este taller contendra diferentes </div>
                     <div className='requerimientos'>req</div>
